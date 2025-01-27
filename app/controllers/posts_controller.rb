050be46286ba518eadad50b0c98cbccdb4e3177a
MatsumuraYuuki <rikkyo.matumura@gmail.com>
2025-01-27 19:31:57 +0900

    学習ログ一覧機能を作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   # bookroom2では投稿の閲覧についてはログインを必要としないことにする
-  before_action :authenticate_user!, except: [:show]
+  before_action :authenticate_user!, except: [:show, :index]
 
   def new
     @post = Post.new
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
     @post.user_id = current_user.id # ログインユーザのIDを代入
     if @post.save
       flash[:notice] = '投稿しました'
-      redirect_to root_path # 一時的にトップページへリダイレクト(要修正)
+      redirect_to posts_path
     else
       flash[:alert] = '投稿に失敗しました'
       render :new
@@ -20,6 +20,10 @@ class PostsController < ApplicationController
 
   def show # 追加
     @post = Post.find_by(id: params[:id])
+  end
+
+  def index # 追加
+    @posts = Post.limit(10).order(created_at: :desc)
   end
 
 
