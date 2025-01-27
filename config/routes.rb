f3e618f6dcc2f1e8ac4cf673c194becf4273bd19
MatsumuraYuuki <rikkyo.matumura@gmail.com>
2025-01-27 19:56:39 +0900

    学習ログ削除機能を作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#top'
 
-  resources :posts, only: [:new, :create, :show, :index]
+  resources :posts, only: [:new, :create, :show, :index, :destroy]
 
 end
