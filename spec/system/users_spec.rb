d24dda7f3874565d1ea136ab11ac5f3116da9d3d
MatsumuraYuuki <rikkyo.matumura@gmail.com>
2025-01-27 12:01:32 +0900

    ユーザー登録画面の修正(異常系テストの追加)

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -28,5 +28,63 @@ describe 'User', type: :system do
         expect(current_path).to eq('/') # ユーザー登録後はトップページにリダイレクト
       end
     end
+
+    context '異常系' do
+      context 'user_nameが空の場合' do
+        let(:user_name) { '' }
+        it 'ユーザーを作成せず、エラーメッセージを表示する' do
+          expect { subject }.not_to change(User, :count) # Userが増えない
+          expect(page).to have_content("User name can't be blank") # エラーメッセージのチェック
+        end
+      end
+
+      context 'user_nameが20文字を超える場合' do
+        let(:user_name) { 'あ' * 21 }
+        it 'ユーザーを作成せず、エラーメッセージを表示する' do
+          expect { subject }.not_to change(User, :count)
+          expect(page).to have_content('User name is too long (maximum is 20 character')
+        end
+      end
+
+      context 'emailが空の場合' do
+        let(:email) { '' }
+        it 'ユーザーを作成せず、エラーメッセージを表示する' do
+          expect { subject }.not_to change(User, :count)
+          expect(page).to have_content("Email can't be blank")
+        end
+      end
+
+      context 'passwordが空の場合' do
+        let(:password) { '' }
+        it 'ユーザーを作成せず、エラーメッセージを表示する' do
+          expect { subject }.not_to change(User, :count)
+          expect(page).to have_content("Password can't be blank")
+        end
+      end
+
+      context 'passwordが6文字未満の場合' do
+        let(:password) { 'a' * 5 }
+        it 'ユーザーを作成せず、エラーメッセージを表示する' do
+          expect { subject }.not_to change(User, :count)
+          expect(page).to have_content('Password is too short (minimum is 6 characters')
+        end
+      end
+
+      context 'passwordが128文字を超える場合' do
+        let(:password) { 'a' * 129 }
+        it 'ユーザーを作成せず、エラーメッセージを表示する' do
+          expect { subject }.not_to change(User, :count)
+          expect(page).to have_content('Password is too long (maximum is 128 characters)')
+        end
+      end
+
+      context 'passwordとpassword_confirmationが一致しない場合' do
+        let(:password_confirmation) { "#{password}hoge" } # passwordに"hoge"を足した文字列にする
+        it 'ユーザーを作成せず、エラーメッセージを表示する' do
+          expect { subject }.not_to change(User, :count)
+          expect(page).to have_content("Password confirmation doesn't match Password")
+        end
+      end
+    end
   end
 end
