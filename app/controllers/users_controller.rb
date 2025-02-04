17e3601ced88e1d058e47687342411d0986259dd
MatsumuraYuuki <rikkyo.matumura@gmail.com>
2025-02-04 16:20:19 +0900

    ユーザーが見つからなかった場合のエラーハンドリングを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :show] # ログイン必須
 
-  #/users/13232・/users/indexなどの存在しないIDにアクセスしようとした際に、Railsがindexをidパラメータとして解釈しようとして発生しまうエラーが存在している
+  #レコードが見つからない場合にnilを返す
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by(id: params[:id])
+    if @user.nil?
+      flash[:alert] = 'ユーザーが見つかりません'
+      redirect_to users_path
+    end
   end
 
   def index
