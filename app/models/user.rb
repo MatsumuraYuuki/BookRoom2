042377d65e2a1896c14e0353fb9cf7560835fb46
MatsumuraYuuki <rikkyo.matumura@gmail.com>
2025-02-06 18:56:38 +0900

    RememberMe機能とプロフ編集ページを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, #DBに保存されたpassが正しいかどうかの検証
          :registerable, #基本的にUser登録、編集、削除機能を作成することができる
          :recoverable, #パスワードをリセットする 
-         :rememberable, #20日間ログインしたままにすると言った、永続ログイン機能を作成
+         :rememberable, #20日間ログインしたままにすると言った、remembeme機能を作成
          :validatable #emailのフォーマットやpassの長さなど、一般的なバリデーションを追加
 
   validates :user_name, presence: true, length: { maximum: 20 } # 変更
